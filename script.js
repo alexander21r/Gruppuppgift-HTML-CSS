fb36ab03faeec14318bc2457d911815890be3107
FredrikFalto <faltofredrik@gmail.com>
2021-11-30 10:47:44 +0100

    Bytte namn på en kniv, ändrade jumbotron storlek

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class Knife {
-    constructor (name, blade, handle, deliveryTime) {
+    constructor(name, blade, handle, deliveryTime) {
         this.name = name;
         this.blade = blade;
         this.handle = handle;
@@ -10,7 +10,7 @@ class Knife {
 let knives = [];
 
 knives.push(new Knife("Kockkniv 180", "180 mm långt och består av 210 lager damaskusstål.", "Valnöt", "8-10 veckor"));
-knives.push(new Knife("Kockkniv 140", "140 mm långt och består av 210 lager damaskusstål.", "Valnöt", "6-8 veckor"));
+knives.push(new Knife("Santoku 140", "140 mm långt och består av 210 lager damaskusstål.", "Valnöt", "6-8 veckor"));
 knives.push(new Knife("Trancherkniv 160", "160 mm långt och består av 120 lager damaskusstål.", "Valnöt", "3-4 veckor"));
 knives.push(new Knife("Skalkniv 90", "90 mm långt och består av 140 lager damaskusstål.", "Valnöt", "6-8 veckor"));
 knives.push(new Knife("Brödkniv 270", "270 mm långt och består av 210 lager damaskusstål.", "Valnöt", "2-4 veckor"));
@@ -28,14 +28,14 @@ function moreInfoChef(x) {
 // Bladet har 210 lager Damaskusstål, som skapar dess vackra mönster.
 
 // Funktion för karta Visa/Göm
-function hideShowMap(){
+function hideShowMap() {
     var x = document.getElementById("map");
     var y = document.getElementById("mapButton");
-    if(x.style.display === "none"){
+    if (x.style.display === "none") {
         x.style.display = "block";
         document.getElementById("mapButton").innerHTML = "Göm karta"
 
-    }else{
+    } else {
         x.style.display = "none";
         document.getElementById("mapButton").innerHTML = "Visa karta"
     }
